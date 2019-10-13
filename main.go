994067e9ed60cb36ca756f3939759749a9eaf8cc
OhYee <oyohyee@oyohyee.com>
2019-10-14 01:29:42 +0800

    misc: main file add AES(#master)

--- main.go
+++ main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"github.com/OhYee/cryptography_and_network_security/DES"
 	"fmt"
+	"github.com/OhYee/cryptography_and_network_security/AES"
+	"github.com/OhYee/cryptography_and_network_security/DES"
 	"os"
 )
 
@@ -21,6 +22,8 @@ func exgcd(a int, b int) (r int, x int, y int) {
 func main() {
 	des.Logger.SetOutput(os.Stdout)
 	fmt.Printf("%016x\n", des.DES(0x02468ACEECA86420, 0x0F1571C947D9E859))
+	aes.Logger.SetOutput(os.Stdout)
+	fmt.Printf("%s\n", aes.AES("0123456789abcdeffedcba9876543210", "0f1571c947d9e8590cb7add6af7f6798"))
 	r, x, y := exgcd(97, 35)
 	fmt.Printf("%d %d %d\n", r, x, y)
 }
