b0527515a751518f160aa44b0e9ea9aa00e72cfe
OhYee <oyohyee@oyohyee.com>
2019-10-15 00:25:51 +0800

    misc: GF(2^n) demo(#master)

--- main.go
+++ main.go
@@ -4,26 +4,19 @@ import (
 	"fmt"
 	"github.com/OhYee/cryptography_and_network_security/AES"
 	"github.com/OhYee/cryptography_and_network_security/DES"
+	"github.com/OhYee/cryptography_and_network_security/GF"
+	"github.com/OhYee/cryptography_and_network_security/GF/euclid"
 	"os"
 )
-
-func exgcd(a int, b int) (r int, x int, y int) {
-	if b == 0 {
-		return a, 1, 0
-	}
-	r, x, y = exgcd(b, a%b)
-	t := x
-	x = y
-	y = t - a/b*y
-	fmt.Printf("%d * %d + %d * %d = %d\n", a, x, b, y, r)
-	return r, x, y
-}
 
 func main() {
 	des.Logger.SetOutput(os.Stdout)
 	fmt.Printf("%016x\n", des.DES(0x02468ACEECA86420, 0x0F1571C947D9E859))
 	aes.Logger.SetOutput(os.Stdout)
 	fmt.Printf("%s\n", aes.AES("0123456789abcdeffedcba9876543210", "0f1571c947d9e8590cb7add6af7f6798"))
-	r, x, y := exgcd(97, 35)
+	euclid.Logger.SetOutput(os.Stdout)
+	r, x, y := euclid.ExGCD(97, 35, euclid.Plus, euclid.Multiplus, euclid.Divide)
+	fmt.Printf("%d %d %d\n", r, x, y)
+	r, x, y = euclid.ExGCD(0b100011011, 0b10000011, gf.Plus, gf.Multiplus, gf.Divide)
 	fmt.Printf("%d %d %d\n", r, x, y)
 }
