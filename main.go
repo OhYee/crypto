faa081ae1886ef8f5f328e6590378cc555e6bb13
OhYee <oyohyee@oyohyee.com>
2020-05-29 00:26:03 +0800

    misc: rename des(#master)

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ import (
 
 func main() {
 	des.Logger.SetOutput(os.Stdout)
-	fmt.Printf("%016x\n", des.DES(0x02468ACEECA86420, 0x0F1571C947D9E859))
+	fmt.Printf("%016x\n", des.Encrypto(0x02468ACEECA86420, 0x0F1571C947D9E859))
 	aes.Logger.SetOutput(os.Stdout)
 	fmt.Printf("%s\n", aes.AES("0123456789abcdeffedcba9876543210", "0f1571c947d9e8590cb7add6af7f6798"))
 	euclid.Logger.SetOutput(os.Stdout)
